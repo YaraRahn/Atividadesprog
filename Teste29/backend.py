c3d8c666437d067babd7bc53e383bfd87081ecce
Yara Rahn <101571988+YaraRahn@users.noreply.github.com>
2022-08-29 10:59:59 -0300

    Add files via upload
    Backend do teste de carga.

---
+++ Teste29/backend.py
@@ -0,0 +1,67 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+# https://dev.to/thetrebelcc/how-to-run-a-flask-app-over-https-using-waitress-and-nginx-2020-235c
+from waitress import serve
+
+path = os.path.dirname(os.path.abspath(__file__)) 
+arquivobd = os.path.join(path, 'pessoa.db')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///"+arquivobd
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+cont = 0
+
+class Pessoa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(254))
+    email = db.Column(db.String(254))
+    telefone = db.Column(db.String(254))
+
+    def __str__(self):
+        return self.nome + "[id="+str(self.id)+ "], " +\
+            self.email + ", " + self.telefone
+
+    def json(self):
+        return { "id": self.id, 
+                 "nome": self.nome, 
+                 "email": self.email, 
+                 "telefone": self.telefone
+               }
+
+@app.route("/")
+def ola():
+    return "Servidor backend operante"
+
+@app.route("/listar")
+def listar():
+    pessoas = db.session.query(Pessoa).all()
+    lista = []
+    for p in pessoas:
+        lista.append(p.json())
+    resposta = jsonify(lista)
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta
+
+@app.route("/operacao")
+def operacao():
+    global cont
+    cont += 1
+    print("contador: ", cont)
+    return "A operação foi executada, e o contador foi incrementado"
+
+@app.route("/contador")
+def contador():
+    return str(cont)
+
+@app.route("/info")
+def nomes():
+    return "Alana, Manu, Lari, Liriel, Yara"
+
+
+#app.run(debug=True)
+serve(app, host='0.0.0.0', port=5000)
+
+# teste: curl localhost:5000/listar
